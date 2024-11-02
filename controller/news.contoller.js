738ea50c7cfe01acd579850f0bd4f8df3687148b
MohammedHassan07 <mhassancs260@gmail.com>
2024-11-02 12:24:57 +0530

    FEAT: get by id

--- controller/news.contoller.js
+++ controller/news.contoller.js
@@ -38,8 +38,29 @@ async function getByCategory(req, res) {
     }
 }
 
+async function getById(req, res) {
+    try {
+
+        const newsId = Number(req.params.id)
+
+        const news = await prisma.post.findMany({
+            where: {
+                id: newsId
+            }
+        })
+
+        if (!news) return res.status(404).json({ message: 'No post found' })
+
+        res.status(200).json({ news })
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({ message: 'Server Error' })
+    }
+}
+
 module.exports = {
 
     getAllNews,
-    getByCategory
+    getByCategory,
+    getById
 }
--- routes/news.routes.js
+++ routes/news.routes.js
@@ -1,10 +1,12 @@
 const express = require('express')
-const { getAllNews, getByCategory } = require('../controller/news.contoller')
+const { getAllNews, getByCategory, getById } = require('../controller/news.contoller')
 
 const route = express.Router()
 
 route.get('/all', getAllNews)
 
 route.get('/by-category/:category', getByCategory)
+
+route.get('/by-id/:id', getById)
 
 module.exports = route
