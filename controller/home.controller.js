669f855700e82f35466eafafa3acde721f9a22c0
MohammedHassan07 <mhassancs260@gmail.com>
2024-11-02 12:45:35 +0530

    LAYOUT: added bootstrap

--- controller/home.controller.js
+++ controller/home.controller.js
@@ -1,6 +1,19 @@
-function home(req, res) {
+const prisma = require('../config/prismaClient')
+async function home(req, res) {
 
-    res.render('home')
+    try {
+
+        const news = await prisma.post.findMany()
+
+        if (!news) return res.status(404).json({ message: 'No post found' })
+
+        // console.log(news)
+        res.render('home', {news})
+
+    } catch (error) {
+
+
+    }
 }
 
 function contact(req, res) {
